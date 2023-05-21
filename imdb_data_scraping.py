dda34b0982789abbc037c8607d00d2f8bedbe68c
ozgecinko <cinkoozge@gmail.com>
2023-05-22 01:11:38 +0300

    added year value and filtered only movies

--- imdb_data_scraping.py
+++ imdb_data_scraping.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', 500)
@@ -33,12 +34,14 @@ def scrape_movies_for_keyword(weather, keyword):
     directors = []
     votess = []
     grosses = []
+    years = []
 
     base_url = "https://www.imdb.com/search/keyword/"
     params = {
         "keywords": keyword,
         "sort": "moviemeter,asc",
-        "mode": "detail"
+        "mode": "detail",
+        "title_type": "movie",
     }
 
     page = 1
@@ -53,8 +56,9 @@ def scrape_movies_for_keyword(weather, keyword):
 
         for movie_element in movie_elements:
             year_element = movie_element.find("span", class_="lister-item-year text-muted unbold")
-            if year_element and ("Video Game" in year_element.text or "TV Series" in year_element.text):
-                continue  # Skip video games and TV series
+            year_text = year_element.text if year_element else ""
+            years.append(year_text)
+
             title_element = movie_element.find("h3", class_="lister-item-header")
             title = title_element.a.text.strip() if title_element else "N/A"
             titles.append(title)
@@ -87,6 +91,7 @@ def scrape_movies_for_keyword(weather, keyword):
 
     data = {
         "Title": titles,
+        "Year": years,
         "Genre": genres,
         "Rating": ratings,
         "Description": descriptions,
@@ -101,18 +106,16 @@ def scrape_movies_for_keyword(weather, keyword):
     return df
 
 
-
 keyword_map = {
-    "Rain": ["rain", "rainy-day", "rainy-weather", "melancholy", "coziness"],
-    "Clear": ["clear-sky", "sun", "sunny", "sunny-day", "sunny-weather", "happiness", "optimism"],
-    "Clouds": ["cloud","cloudy-sky", "cloudiness", "serenity", "contemplation"],
-    "Drizzle": ["rainfall", "raindrop", "tranquility", "serenity"],
-    "Thunderstorm": ["thunderstorm", "storm", "stormy-weather", "awe", "excitement"],
+    "Rain": ["rain", "rainy-day", "rainy-weather", "melancholy", "coziness", "love-story"],
+    "Clear": ["clear-sky", "sun", "sunny", "sunny-day", "sunny-weather", "happiness", "optimism", "inspirational"],
+    "Clouds": ["cloud","cloudy-sky", "cloudiness", "daydream", "dream", "contemplation"],
+    "Drizzle": ["rainfall", "raindrop", "tranquility", "serenity", "gentleman", "gentlewoman", "peace"],
+    "Thunderstorm": ["thunderstorm", "storm", "stormy-weather", "excitement", "hate", "revenge"],
     "Snow": ["snow", "snowing", "winter", "christmas", "wonder", "playfulness", "holiday"],
-    "Mist": ["mist", "misty-day", "intrigue", "calmness", "calm"],
-    "Fog": ["fog", "foggy-night", "uncertainty", "introspection"]
+    "Mist": ["mist", "misty-day", "intrigue", "calmness", "calm", "thriller"],
+    "Fog": ["fog", "foggy-night", "uncertainty", "introspection", "psychological-thriller"]
 }
-
 
 
 movie_dfs = []
@@ -123,4 +126,4 @@ for weather, keywords in keyword_map.items():
 all_movies_df = pd.concat(movie_dfs, ignore_index=True)
 
 print(all_movies_df)
-all_movies_df.to_csv('imdb_weather_data2.csv')
+all_movies_df.to_csv('imdb_weather_data.csv')
